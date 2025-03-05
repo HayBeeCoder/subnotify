246d91912b885a911392273f38e206951ac178d0
HayBeeCoder <designerytg@skiff.com>
2025-03-05 21:58:16 +0100

    [feature] add supabase composable

---
+++ src/composables/useSupabase.ts
@@ -0,0 +1,27 @@
+// UseSupabase.js
+import { createClient } from '@supabase/supabase-js'
+import useAuthUser from './useAuthUser'
+
+// these can come from an environment variable if desired
+// not required however as they are 100% exposed on the client side anyway
+// and that's ok, Supabase expects this (security is provided by Row Level Security)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+// setup client
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+})
+
+supabase.auth.onAuthStateChange((event, session) => {
+  const { user } = useAuthUser()
+
+  user.value = session?.user || null
+})
+
+// expose supabase client
+export default function useSupabase() {
+  return { supabase }
+}
