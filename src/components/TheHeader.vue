e5ae561c56e154acf2baeb3cf97a729711f38b9b
HayBeeCoder <designerytg@skiff.com>
2025-03-05 13:39:00 +0100

    [feature] create the header standalone component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import IconSubnotify from './icons/IconSubnotify.vue'
+</script>
+
+<template>
+  <header class="fixed w-screen px-6 min-md:px-10 pt-6 min-md:pt-6 z-10000">
+    <RouterLink to="/">
+      <span
+        :class="{
+          'text-[#FF5E3A] block min-md:w-24 w-24': true,
+          ...{ 'text-white': $route.path == '/' },
+        }"
+      >
+        <IconSubnotify />
+      </span>
+    </RouterLink>
+  </header>
+</template>
