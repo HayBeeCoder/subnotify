2a8ab8e0b80461a768c8fdd595944f015c664dce
HayBeeCoder <designerytg@skiff.com>
2025-03-05 13:36:50 +0100

    [refactor] replace the home view content with expected code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ProductPitch from '@/components/pitch/ProductPitch.vue'
+import LoginView from './LoginView/LoginView.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="w-screen h-screen relative flex">
+    <section
+      class="min-md:w-1/2 min-md:relative bg-[#CC492C] flex justify-center absolute top-0 left-0 w-screen h-screen"
+    >
+      <ProductPitch />
+    </section>
+    <section class="hidden min-md:block h-full">
+      <LoginView />
+    </section>
+  </section>
 </template>
