440c8d929d7c38e74c947b4cbbda9b24aaf1c6bb
HayBeeCoder <designerytg@skiff.com>
2025-03-05 13:37:27 +0100

    [feature] add the auth route

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/auth',
+      name: 'login',
+      component: () => import('../views/LoginView/LoginView.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
