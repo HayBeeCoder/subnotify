5f00d529edba9679e736b8f869b61539d8f4810a
HayBeeCoder <designerytg@skiff.com>
2025-03-06 21:00:43 +0100

    [feature] begin implementation of auth logic

--- src/composables/useAuthUser.ts
+++ src/composables/useAuthUser.ts
@@ -1,26 +1,12 @@
 import { ref, type Ref } from 'vue'
 import useSupabase from './useSupabase'
-import type { Provider, User } from '@supabase/supabase-js'
+import type { Provider, Session, User } from '@supabase/supabase-js'
 
-// user is set outside of the useAuthUser function
-// so that it will act as global state and always refer to a single user
 const user: Ref<User | null> = ref(null)
 
 export default function useAuthUser() {
   const { supabase } = useSupabase()
 
-  /**
-   * Login with email and password
-   */
-  // const login = async ({ email, password }: { email: string; password: string }) => {
-  //   const { user, error } = await supabase.auth.signIn({ email, password })
-  //   if (error) throw error
-  //   return user
-  // }
-
-  /**
-   * Login with google, github, etc
-   */
   const loginWithSocialProvider = async (provider: Provider) => {
     try {
       console.log({ start: 1 })
@@ -39,42 +25,57 @@ export default function useAuthUser() {
     return
   }
 
-  /**
-   * Logout
-   */
   const logout = async () => {
     const { error } = await supabase.auth.signOut()
     if (error) throw error
   }
 
-  /**
-   * Check if the user is logged in or not
-   */
-  const isLoggedIn = (): boolean => {
-    if (!user.value) {
-      supabase.auth.getSession().then(({ data }) => {
-        console.log('User session:', data.session)
-        return
-      })
+  const getUser = async () => {
+    const { data, error } = await supabase.auth.getUser()
+    if (error) {
+      console.log('Error fetching user:', error)
+    } else {
+      console.log('User deets: ', data.user)
     }
-    return true
+    return data.user
   }
 
-  /**
-   * Register
-   */
-  // const register = async ({ email, password, ...meta }) => {}
+  async function refreshToken(session: Session) {
+    const { data, error } = await supabase.auth.refreshSession({
+      refresh_token: session.refresh_token,
+    })
+    if (error) console.log('Error refreshing access token: ', error)
+    return data
+  }
 
-  /**
-   * Update user email, password, or meta data
-   */
-  // const update = async (data) => {}
+  function isAccessTokenExpired(session: Session) {
+    if (!session || !session.expires_at) return true // No session, assume expired
 
-  /**
-   * Send user an email to reset their password
-   * (ie. support "Forgot Password?")
-  //  */
-  // const sendPasswordRestEmail = async (email) => {}
+    const currentTime = Math.floor(Date.now() / 1000) // Current
+
+    return currentTime >= session.expires_at
+  }
+  const isLoggedIn = async () => {
+    const { data, error } = await supabase.auth.getSession()
+    if (error) return false
+
+    const is_access_token_expired = isAccessTokenExpired(data.session as Session)
+    if (!data.session) {
+      return false
+    }
+    if (is_access_token_expired) {
+      const refresh_token_response = await refreshToken(data.session as Session)
+      if (refresh_token_response.session || refresh_token_response.user) {
+        user.value = refresh_token_response.user
+      }
+    } else {
+      user.value = await getUser()
+    }
+
+    console.log({ user: user.value })
+    alert('user spotted!')
+    return true
+  }
 
   return {
     user,
