eddbe83d29c44e11b71037b593ed8510c6e39cd0
HayBeeCoder <haybeecodes@gmail.com>
2025-03-07 20:00:01 +0100

    [feature] build the link variant of TheButton

--- src/components/TheButton/TheButton.vue
+++ src/components/TheButton/TheButton.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 import type { TColors, TSizes } from './types'
 
 const props = defineProps<{
-  variant: 'primary' | 'secondary' | 'neutral'
+  variant: 'primary' | 'secondary' | 'neutral' | "link"
   size: 'small' | 'medium' | 'large'
   fillupXaxis?: boolean
   iconExists?: boolean
@@ -16,22 +16,26 @@ const COLORS: TColors = {
   primary: '',
   secondary: '',
   neutral: 'bg-white text-[#CC492C]',
+  link: "bg-transparent hover:border-b-4 hover:border-[#CC492C] hover:bg-transparent "
 }
 
 const color = ref({
   [COLORS.neutral]: props.variant == 'neutral',
   [COLORS.primary]: props.variant == 'primary',
   [COLORS.secondary]: props.variant == 'secondary',
+  [COLORS.link]: props.variant == 'link',
 })
 
 const SIZES: TSizes = {
-  small: '',
-  medium: '',
+  small: 'text-sm py-1',
+  medium: 'font-bold text-md mx-4 min-md:px-8 py-3 rounded-xs',
   large: 'font-bold text-lg px-6 min-md:px-10 py-4 rounded-sm  ',
 }
 
 const size = {
   [SIZES.large]: props.size == 'large',
+  [SIZES.medium]: props.size == 'medium',
+  [SIZES.small]: props.size == 'small',
 }
 
 const width = {
@@ -42,7 +46,7 @@ const width = {
 <template>
   <button
     :class="{
-      'hover:scale-[0.95] hover:bg-gray-100 ease-in-out transition-all duration-150 flex justify-center gap-4 items-center': true,
+      'hover:scale-[0.95] hover:bg-gray-100 underline- ease-in-out transition-all duration-150 flex justify-center gap-4 items-center': true,
       ...color,
       ...width,
       ...size,
