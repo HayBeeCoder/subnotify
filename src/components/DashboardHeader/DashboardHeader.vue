a5b9dc9104509d9f4bf5ce7992e063b5b32e5dd4
HayBeeCoder <haybeecodes@gmail.com>
2025-03-08 19:30:40 +0100

    [feature] implement dashboard header

---
+++ src/components/DashboardHeader/DashboardHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TextinputField from '../InputField.vue/TextinputField.vue'
+import TheButton from '../TheButton/TheButton.vue'
+import IconPlus from '../icons/IconPlus.vue'
+import IconSearch from '../icons/IconSearch.vue'
+
+// const searchquery =  ref("")
+</script>
+
+<template>
+  <section class="flex gap-1 my-4 w-full md:w-full ">
+    <div class="flex items-stretch flex-grow relative">
+      <span class="w-4 h-4 absolute top-1/2 -translate-y-1/2 left-[8px]">
+        <IconSearch/>
+      </span>
+      <TextinputField  size="medium" icon-position="left" placeholder="Search for a subscription"  />
+    </div>
+    <div class="flex items-center justify-end">
+      <TheButton variant="primary" size="medium" customclass=" " icon-exists>
+        <!-- <span class="inline-block"> -->
+          <span class="inline-block h-4 w-4">
+
+            <IconPlus weight="heavy" />
+          </span>
+          <span class="hidden  md:inline-block">
+            Track new subscription
+          </span>
+        <!-- </span> -->
+      </TheButton>
+    </div>
+  </section>
+</template>
