206d4cc8090b7745aab5ce95443aaebdce381413
HayBeeCoder <haybeecodes@gmail.com>
2025-03-10 12:00:37 +0100

    [feature] add modal to dashboard

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import DashboardHeader from '@/components/DashboardHeader/DashboardHeader.vue'
+import TextinputField from '@/components/InputField.vue/TextinputField.vue'
 import TheModal from '@/components/TheModal/TheModal.vue'
 import useAuthUser from '@/composables/useAuthUser'
 import { ref } from 'vue'
 
 const { user } = useAuthUser()
 const isModalOpen = ref(false)
+
+const newSubscription = ref({
+
+})
 </script>
 
 <template>
@@ -24,7 +29,15 @@ const isModalOpen = ref(false)
           isModalOpen = false
         }
       "
-    />
+    >
+    <h2 class="text-center my-4">Track new subscription</h2>
+
+    <form>
+      <!-- <TextinputField  /> -->
+
+      <!-- <TextInputField /> -->
+    </form>
+    </TheModal>
     <div v-if="user">
       <!--user_metadata is the key supabase nests all arbitrary meta data under-->
       <div>Hello {{ user.user_metadata.name }}</div>
