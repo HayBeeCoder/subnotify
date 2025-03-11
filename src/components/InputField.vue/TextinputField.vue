f80770957e2b0bce134090aafbeb32a6ae40517f
HayBeeCoder <haybeecodes@gmail.com>
2025-03-11 16:37:26 +0100

    [feature, refactor] make necessary updates to respective input types

--- src/components/InputField.vue/TextinputField.vue
+++ src/components/InputField.vue/TextinputField.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue'
+import { ref } from 'vue'
 import type { TSizes } from '../types'
 
-const query: Ref<string> = ref('')
 const props = defineProps<{
   placeholder?: string
   size: 'small' | 'medium' | 'large'
@@ -11,7 +10,13 @@ const props = defineProps<{
   longMessage?: boolean
   disabled?: boolean
   type?: 'text' | 'date' | 'number'
+  name: string
+  value: string
 }>()
+
+const query = ref(props.value)
+const startdate = ref(props.value.split('T')[0])
+const duration = ref(props.value)
 
 const SIZES: TSizes = {
   small: '',
@@ -24,12 +29,16 @@ const size = {
   [SIZES.medium]: props.size == 'medium',
   [SIZES.small]: props.size == 'small',
 }
+
+defineEmits(['typeEvent'])
+
 </script>
 
 <template>
   <!-- <form class="flex justify-center my-8" v-on:submit.stop="search"> -->
-  <label v-if="label" class="text-[0.8rem]">{{ label }}</label>
+  <label v-if="label" class="text-[0.8rem]" :for="name">{{ label }}</label>
   <input
+    :name="name"
     v-if="type == 'text'"
     :class="{
       'disabled:opacity-40 inline-block border-slate-300 text-slate-800 focus:outline-none focus:border-slate-500 w-full': true,
@@ -39,10 +48,10 @@ const size = {
         'pl-8': props.iconPosition == 'left',
       },
     }"
-    type="text"
     v-model="query"
+    type="text"
     :placeholder="placeholder"
-    @change="$emit('typeEvent', query)"
+    @input="$emit('typeEvent', query)"
     :disabled="disabled"
   />
 
@@ -52,9 +61,10 @@ const size = {
       'disabled:opacity-40 border-slate-300 text-slate-800 focus:outline-none focus:border-slate-500 w-full': true,
       ...size,
     }"
+    :name="name"
     v-model="query"
     :placeholder="placeholder"
-    @change="$emit('typeEvent', query)"
+    @input="$emit('typeEvent', query)"
     :disabled="disabled"
     rows="4"
     cols="33"
@@ -66,6 +76,11 @@ const size = {
       'disabled:opacity-40 border-slate-300 text-slate-800 focus:outline-none focus:border-slate-500 w-full': true,
       ...size,
     }"
+    @change="$emit('typeEvent', startdate)"
+    :name="name"
+    v-model="startdate"
+    
+    :value="startdate"
     :disabled="disabled"
   />
   <input
@@ -76,7 +91,10 @@ const size = {
       ...size,
     }"
     min="1"
+    :name="name"
+    v-model="duration"
     :disabled="disabled"
+    @input="$emit('typeEvent', duration)"
   />
   <!-- </form> -->
 </template>
