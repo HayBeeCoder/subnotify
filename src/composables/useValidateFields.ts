ce9ae5ae4314d10ea0c213b7896863859b7f98e0
HayBeeCoder <haybeecodes@gmail.com>
2025-03-17 19:50:19 +0100

    [feature] implement functionality for form fields validation

---
+++ src/composables/useValidateFields.ts
@@ -0,0 +1,21 @@
+import type { Validator } from '@/types'
+import validateField from '@/utils/helpers/validateField'
+
+export default function useValidateFields() {
+  const errors: {[key: string | number]: string} = {}
+  function validateForm(form: {[key:string]: string | number}, rules: { [key: string]: Array<Validator> }) {
+    for (const field in rules) {
+      const error = validateField(form[field], rules[field])
+      if (error) {
+        errors[field] = error
+      }
+    }
+    return errors
+  }
+
+  return{
+    validateForm,
+    errors
+  }
+  // const errors = validateForm(form, rules)
+}
