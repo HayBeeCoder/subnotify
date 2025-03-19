9f6190300e672076a3d74fbff6672871da28766f
HayBeeCoder <haybeecodes@gmail.com>
2025-03-19 23:11:46 +0100

    [refactor] delete errors in the errrors object that have been fixed/corrected

--- src/composables/useValidateFields.ts
+++ src/composables/useValidateFields.ts
@@ -2,20 +2,21 @@ import type { Validator } from '@/types'
 import validateField from '@/utils/helpers/validateField'
 
 export default function useValidateFields() {
-  const errors: {[key: string | number]: string} = {}
-  function validateForm(form: {[key:string]: string | number}, rules: { [key: string]: Array<Validator> }) {
+  const errors: { [key: string | number]: string } = {}
+  function validateForm(
+    form: { [key: string]: string | number },
+    rules: { [key: string]: Array<Validator> },
+  ) {
     for (const field in rules) {
       const error = validateField(form[field], rules[field])
-      if (error) {
-        errors[field] = error
-      }
+      if (error) errors[field] = error
+      else delete errors[field]
     }
-    return errors
   }
 
-  return{
+  return {
     validateForm,
-    errors
+    errors,
   }
   // const errors = validateForm(form, rules)
 }
