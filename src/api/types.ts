236d1c854c4bbb7b7e5c7039d67dfa5b41098d53
HayBeeCoder <haybeecodes@gmail.com>
2025-03-22 18:48:23 +0100

    [feature] add api specific types

---
+++ src/api/types.ts
@@ -0,0 +1,21 @@
+import type { AxiosRequestConfig, AxiosResponse } from "axios"
+
+export type TConfig = (AxiosRequestConfig & {
+  initialData: unknown,
+  responseAdapter?: <T>(response: unknown) => T
+})
+
+export type TGet = <T>(url: string, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+export type TPost =  <T>(url: string, body: unknown, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+export type TPatch = <T>(url: string, body: unknown, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+export type TDelete = <T>(url: string, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+export type TPossibleRequestMethods = TGet | TPost | TPatch | TDelete
+export type TParametersforRequestMethods = Parameters<TGet | TPost | TPatch | TDelete>
+
+export interface TApi {
+  get: <T>(url: string, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+  post: <T>(url: string, body?: unknown, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+  patch: <T>(url: string, body?: unknown, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+  delete: <T>(url: string, config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>
+}
+
