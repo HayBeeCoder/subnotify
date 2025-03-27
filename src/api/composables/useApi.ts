a3e4ef3976b147edbf6282fff902065494e43511
HayBeeCoder <haybeecodes@gmail.com>
2025-03-27 23:35:55 +0100

    [feature]

--- src/api/composables/useApi.ts
+++ src/api/composables/useApi.ts
@@ -2,38 +2,48 @@ import { ref, computed, type ComputedRef, type Ref } from 'vue'
 import upperFirst from '../helpers/upperFirst'
 import { apiStatus } from '../constants/apiStatus'
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
-import type {  TConfig, TDelete, TGet, TPatch, TPossibleRequestMethods, TPost  } from '../types'
+import type { TConfig, TDelete, TGet, TPatch, TPossibleRequestMethods, TPost } from '../types'
 
 const { IDLE, SUCCESS, PENDING, ERROR } = apiStatus
 
-type TNormalizedApiStatuses = {
-  [key: string]: ComputedRef<boolean>
-}
+type TNormalizedApiStatuses<T extends string> = {
+  [K in T]: ComputedRef<boolean>;
+};
 
+const createNormalisedApiStatuses = <T extends Record<string, symbol>>(
+  status: Ref<symbol>,
+  apiName: string
+  // apiStatus: T
+): TNormalizedApiStatuses<`${string}Status${Capitalize<keyof T & string>}`> => {
+  const normalisedApiStatuses = {} as TNormalizedApiStatuses<
+    `${string}Status${Capitalize<keyof T & string>}`
+  >;
 
-const createNormalisedApiStatuses = (status: Ref<symbol, symbol>, apiName: string) => {
-  const normalisedApiStatuses: TNormalizedApiStatuses = {}
   for (const [statusKey, statusValue] of Object.entries(apiStatus)) {
-    let propertyName = ''
+    let propertyName = '';
+
     // Create a property name for each computed status
     if (apiName) {
-      propertyName = `${apiName}Status${upperFirst(statusKey.toLowerCase())}`
+      propertyName = `${apiName}Status${upperFirst(statusKey.toLowerCase())}`;
     } else {
-      propertyName = `status${statusKey.toLowerCase()}`
+      propertyName = `status${statusKey.toLowerCase()}`;
     }
-    // Create a computed that returns true/false based on
-    // the currently selected status
-    normalisedApiStatuses[propertyName] = computed(() => statusValue === status.value)
+
+    // Create a computed that returns true/false based on the currently selected status
+    normalisedApiStatuses[propertyName as keyof typeof normalisedApiStatuses] = computed(
+      () => statusValue === status.value
+    );
   }
-  return normalisedApiStatuses
-}
+
+  return normalisedApiStatuses;
+};
 /**
  * @param {string} apiName
  * @param {function} fn
  * @param {object} config
  */
-export const useApi = <T>(apiName: string, fn: TPossibleRequestMethods, config: TConfig) => {
-  const { initialData, responseAdapter } = config
+export const useApi = <T>(apiName: string, fn: TPossibleRequestMethods, config?: TConfig) => {
+  const { initialData, responseAdapter } = config as TConfig
   // Reactive values to store data and API status
   const data = ref(initialData as T)
   const status = ref(IDLE)
@@ -48,19 +58,20 @@ export const useApi = <T>(apiName: string, fn: TPossibleRequestMethods, config: 
       // API request starts
       status.value = PENDING
       // const response = await fn<T>(...args)
-      let response:  AxiosResponse<T, unknown> | undefined
+      let response: AxiosResponse<T, unknown> | undefined
       const secondargItem = args[1] as AxiosRequestConfig
-      const hasBody = args.length >= 2 && typeof args[1] !== 'object' && !('headers' in secondargItem );
+      const hasBody =
+        args.length >= 2 && typeof args[1] !== 'object' && !('headers' in secondargItem)
 
-    if (hasBody) {
-      // POST or PATCH request: [url, body, config?]
-      const [url, body, config] = args;
-      response = await (fn as TPost | TPatch)(url, body, config);
-    } else {
-      // GET or DELETE request: [url, config?]
-      const [url, config] = args;
-      response = await (fn as TGet | TDelete)(url, config as AxiosRequestConfig);
-    }
+      if (hasBody) {
+        // POST or PATCH request: [url, body, config?]
+        const [url, body, config] = args
+        response = await (fn as TPost | TPatch)(url, body, config)
+      } else {
+        // GET or DELETE request: [url, config?]
+        const [url, config] = args
+        response = await (fn as TGet | TDelete)(url, config as AxiosRequestConfig)
+      }
       // Before assigning the response, check if a responseAdapter
       // was passed, if yes, then use it
       data.value = typeof responseAdapter === 'function' ? responseAdapter<T>(response) : response
@@ -72,11 +83,13 @@ export const useApi = <T>(apiName: string, fn: TPossibleRequestMethods, config: 
       status.value = ERROR
     }
   }
+
+  const normalized = createNormalisedApiStatuses(status, apiName)
   return {
     data,
     status,
     error,
     exec,
-    ...createNormalisedApiStatuses(status, apiName),
+    ...normalized,
   }
 }
