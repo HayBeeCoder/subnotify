9defd05c1d6f3245c58482ce081d4e5a379db9f1
HayBeeCoder <haybeecodes@gmail.com>
2025-03-31 14:21:10 +0100

    [feature] add disabled functionality

--- src/components/SelectField.vue/SelectField.vue
+++ src/components/SelectField.vue/SelectField.vue
@@ -7,6 +7,7 @@ defineProps<{
   options: SelectOption[]
   id: string
   label?: string
+  disabled: boolean
 }>()
 
 const emit = defineEmits(['select'])
@@ -25,23 +26,27 @@ const selectOption = (option: { label: string; value: string }) => {
 <template>
   <div class="relative w-full max-w-sm">
     <label v-if="label" class="text-[0.8rem]" >{{ label }}</label>
-  
+
     <!-- Trigger Button -->
     <button
+      type="button"
       @click.prevent="toggleDropdown"
-      class="block w-full rounded-lg border border-gray-300 bg-white px-4 py-2 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 sm:text-sm"
+      class="relative disabled:opacity-40 block w-full rounded-lg border border-gray-300 bg-white pl-3 py-2 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 sm:text-sm"
       :aria-expanded="isOpen"
       :aria-controls="'dropdown-' + id"
+      :disabled
     >
       {{ selectedOption.label || 'Select an option' }}
-      <span class="float-right text-gray-500">
+      <span class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 inline-flex items-baseline ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
+          class="h-4 w-4 flex items-end"
           viewBox="0 0 20 20"
           fill="currentColor"
+
         >
           <path
+          class="inline-block"
             fill-rule="evenodd"
             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
             clip-rule="evenodd"
