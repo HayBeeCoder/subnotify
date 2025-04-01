598d8f23d80cfb052f749e5091c33a111f5f0a6f
HayBeeCoder <haybeecodes@gmail.com>
2025-04-02 00:34:11 +0100

    [feature] add sorter to dashboard view

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,38 +1,39 @@
 <script setup lang="ts">
 import DashboardCards from '@/components/DashboardCards/DashboardCards.vue'
 import DashboardHeader from '@/components/DashboardHeader/DashboardHeader.vue'
+import TheSorter from '@/components/TheSorter/TheSorter.vue'
 
 import useAuthUser from '@/composables/useAuthUser'
 import { ref } from 'vue'
 
-
 const { user } = useAuthUser()
 
-const query = ref("")
+const query = ref('')
 
 // const newSubscription = ref({})
 </script>
 
 <template>
-  
   <section class="px-6 w-screen text-wrap max-w-full overflow-x-hidden break-words">
     <div v-if="user">
       <!--user_metadata is the key supabase nests all arbitrary meta data under-->
       <p>Hello {{ user.user_metadata.name }}</p>
-      <p>Searching for {{ query  }}</p>
+      <p>Searching for {{ query }}</p>
     </div>
     <DashboardHeader
       :query="query"
       @type-event="
-        (value:string) => {
-          query=value
+        (value: string) => {
+          query = value
         }
       "
     />
 
 
+<div class="flex justify-end"> 
+    <TheSorter />
+ </div>
 
-    <DashboardCards>
-    </DashboardCards>
+    <DashboardCards> </DashboardCards>
   </section>
 </template>
