043731781c08694702b4d06584f5bddd63676800
HayBeeCoder <haybeecodes@gmail.com>
2025-04-01 10:26:38 +0100

    [feature] implement susbcription card

--- src/components/DashboardCards/DashboardCard.vue
+++ src/components/DashboardCards/DashboardCard.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
 import convertTimezone from '@/utils/helpers/convertTime'
 import type { CardType } from './type'
+import IconEdit from '../icons/IconEdit.vue';
+import IconBell from '../icons/IconBell.vue';
+import IconmoreVertical from '../icons/IconmoreVertical.vue';
 
 defineProps<{
   index: number,
@@ -13,22 +16,25 @@ defineProps<{
 </script>
 
 <template>
-  <li class="mb-5 bg-white p-4 rounded-[4px] border-t-4" :style="{ borderColor: color.pastel }">
+  <li class="mb-5 bg-white p-4 rounded-[4px] border-t-[6px]  md:border-t-[8px] cursor-pointer hover:scale-105 hover:ease-linear ease-linear duration-300" :style="{ borderColor: color.pastel }">
     <div class="flex gap-3 items-center">
       <span class="font-medium text-sm text-[#A9A9A9] font-poppins"> #{{ index + 1 }} </span>
-      <div class="border-[#F5F5F5] border-l-[1px] pl-2">
+      <div class="border-[#F5F5F5] border-l-[1px] pl-2 flex-grow">
         <p>
           <span
-            class="font-bold text-sm md:text-md spacing tracking-wider leading-[100%] text-[#1A1A1A]"
+            class="font-bold text-sm  spacing tracking-wider leading-[100%] text-[#1A1A1A]"
           >
             {{ item.provider }}
           </span>
         </p>
-        <p class="text-[12px] md:text-md leading-[100%]">
+        <p class="text-[12px]  leading-[100%]">
           <!-- type: -->
-          <span class="text-[#A9A9A9]"> {{ item.type }} </span>
+          <span class=" text-[#777777]"> {{ item.type }} </span>
         </p>
       </div>
+      <span class="w-5 h-5 text-[#A9A9A9] hover:text-[#1a1a1a] cursor-pointer">
+      <IconEdit/>
+      </span>
     </div>
 
     <div class="my-3">
@@ -49,14 +55,18 @@ defineProps<{
             ).split(",")[0]
           }}</p>
       </div>
-      <div class="w-full h-[3px] bg-[#F2F2F0] rounded-full flex">
+      <div class="w-full h-[4px] bg-[#d0d0ad] rounded-full flex">
         <div class="w-1/2 h-full rounded-full" :style="{ background: color.dark }" />
       </div>
     </div>
-    <p class="text-[12px] flex gap-2 items-cener">
-      <span class="text-[#A9A9A9]">Notification Date:</span>
-      <span class="text-[#1A1A1A] font-semibold">{{ convertTimezone(item.end_date - 3600, item.user_timezone ) }}</span>
+    <!-- <div class="flex justify-between items-center"> -->
+    <p class="text-[13px] flex items-center ">
+    <span class="w-4 h-4 text-[#a9a9a9] mr-px"><IconBell/></span>
+      <span class="text-[#777777] mr-[2px] ">Notification Date:</span>
+      <span class="text-[#1A1A1A] font-semibold">{{ convertTimezone(item.end_date - 86400, item.user_timezone ) }}</span>
     </p>
+
+    <!-- </div> -->
   </li>
 </template>
 
