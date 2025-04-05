f03f859ca20d67dfc642e71445dcc77e3f251110
HayBeeCoder <haybeecodes@gmail.com>
2025-04-05 01:46:55 +0100

    [feature] modify susbcription form to be in either edit mode ( tryna edit a particular existing subscription) or create mode  ( add an entirely new subscription.

--- src/components/SubscriptionForm/SubscriptionForm.vue
+++ src/components/SubscriptionForm/SubscriptionForm.vue
@@ -5,6 +5,7 @@ import TextinputField from '../InputField.vue/TextinputField.vue'
 import SelectField from '../SelectField.vue/SelectField.vue'
 import TheButton from '../TheButton/TheButton.vue'
 import {
+  areFormsDetailsEqual,
   calculateEndDateFromDuration,
   convertToSeconds,
   getAccurateDateFormat,
@@ -29,6 +30,13 @@ import api from '@/api/api'
 import { useRouter } from 'vue-router'
 import { apiStatus } from '@/api/constants/apiStatus'
 import type { AxiosError } from 'axios'
+import type { CardType } from '../DashboardCards/type'
+
+const emit = defineEmits(["editSuccess"])
+const props = defineProps<{
+  isEditSubscription?: boolean
+  subscriptionToEdit?: CardType
+}>()
 
 const { push } = useRouter()
 
@@ -48,7 +56,7 @@ const {
   error: errorOnSubmission,
   status,
   data: responseFromSubmission,
-} = useApi<CreateSubscriptionResponse>(api.post)
+} = useApi<CreateSubscriptionResponse>(props.isEditSubscription ? api.put : api.post)
 
 const isSubmittingForm = computed(() => status.value == PENDING)
 // const isSubmittingForm = computed(() => true)
@@ -57,13 +65,21 @@ const isSubmittingForm = computed(() => status.value == PENDING)
 //   doesUserKnowsDuration.value = newValue.value == 'duration'
 // })
 
-const subprovider = ref('')
-const subtype = ref('')
-const subdescription = ref('')
+const subprovider = ref(props.subscriptionToEdit?.provider || '')
+const subtype = ref(props.subscriptionToEdit?.type || '')
+const subdescription = ref(props.subscriptionToEdit?.description || '')
 const subdurationtype = ref()
-const subduration = ref('3')
-const substartDate = ref(getAccurateDateFormat(new Date()))
-const subendDate = ref(getEndDate(substartDate.value, 30))
+const subduration = ref(String(props.subscriptionToEdit?.duration) || '3')
+const substartDate = ref(
+  props.subscriptionToEdit
+    ? getAccurateDateFormat(new Date(props.subscriptionToEdit.start_date * 1000))
+    : getAccurateDateFormat(new Date()),
+)
+const subendDate = ref(
+  props.subscriptionToEdit
+    ? getAccurateDateFormat(new Date(props.subscriptionToEdit.end_date * 1000))
+    : getEndDate(substartDate.value, 30),
+)
 
 const rules = {
   [SUBSCRIPTION_PROVIDER]: [required()],
@@ -89,12 +105,21 @@ const handleSubmit = async () => {
     [SUBSCRIPTION_USER_TIMEZONE]: Intl.DateTimeFormat().resolvedOptions().timeZone,
   }
 
+  if (props?.subscriptionToEdit && areFormsDetailsEqual(props.subscriptionToEdit, form)) {
+    alert('no edit was made!')
+    return
+  }
+
   validateForm(form, rules)
   if (!!Object.entries(errors.value).length) {
     return
   }
   try {
-    await exec('/', form)
+    if (props.isEditSubscription) {
+      await exec(`/${(props.subscriptionToEdit as CardType).id as number}`, form)
+    } else
+      await exec('/', form)
+
 
     if (status.value == SUCCESS) {
       alert(responseFromSubmission.value?.message)
@@ -102,12 +127,14 @@ const handleSubmit = async () => {
 
       substartDate.value = getAccurateDateFormat(new Date())
       subendDate.value = getEndDate(substartDate.value, 30)
-      push('/dashboard')
+
+      if (props.isEditSubscription) {
+        emit("editSuccess")
+      } else push('/dashboard')
     }
     if (status.value == ERROR) throw errorOnSubmission.value
   } catch (e: unknown) {
-
-    console.log({error: e})
+    console.log({ error: e })
     alert((e as AxiosError<{ detail: string }>).response?.data?.detail || (e as AxiosError).message)
   }
 
@@ -116,8 +143,9 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-  <section class="my-4 w-full">
-    <h2 class="text-center my-4">Track new subscription</h2>
+  <section class="my-4 w-full" @click="(e) => e.stopPropagation()">
+    <h2 v-if="!isEditSubscription" class="text-center my-4">Track new subscription</h2>
+    <h2 v-else class="text-center my-4">Update subscription</h2>
 
     <form class="mx-5 space-y-3 w-[92%] mx-auto">
       <TextinputField
@@ -178,7 +206,7 @@ const handleSubmit = async () => {
             }
           "
           :maxDate="getAccurateDateFormat(new Date())"
-          :disabled="isSubmittingForm"
+          :disabled="isEditSubscription || isSubmittingForm"
         />
       </div>
 
@@ -229,7 +257,7 @@ const handleSubmit = async () => {
           type="button"
           :disabled="isSubmittingForm"
           @some-event="handleSubmit"
-          >Submit</TheButton
+          >{{ isEditSubscription ? 'Update' : 'Submit' }}</TheButton
         >
       </div>
     </form>
