a99204b66b623b0672c498a98dae4ad752e1e813
HayBeeCoder <haybeecodes@gmail.com>
2025-04-06 19:07:25 +0100

    [feature]

--- src/components/SubscriptionForm/SubscriptionForm.vue
+++ src/components/SubscriptionForm/SubscriptionForm.vue
@@ -32,7 +32,7 @@ import { apiStatus } from '@/api/constants/apiStatus'
 import type { AxiosError } from 'axios'
 import type { CardType } from '../DashboardCards/type'
 
-const emit = defineEmits(["editSuccess"])
+const emit = defineEmits(['editSuccess'])
 const props = defineProps<{
   isEditSubscription?: boolean
   subscriptionToEdit?: CardType
@@ -117,9 +117,7 @@ const handleSubmit = async () => {
   try {
     if (props.isEditSubscription) {
       await exec(`/${(props.subscriptionToEdit as CardType).id as number}`, form)
-    } else
-      await exec('/', form)
-
+    } else await exec('/', form)
 
     if (status.value == SUCCESS) {
       alert(responseFromSubmission.value?.message)
@@ -129,7 +127,7 @@ const handleSubmit = async () => {
       subendDate.value = getEndDate(substartDate.value, 30)
 
       if (props.isEditSubscription) {
-        emit("editSuccess")
+        emit('editSuccess')
       } else push('/dashboard')
     }
     if (status.value == ERROR) throw errorOnSubmission.value
@@ -143,42 +141,43 @@ const handleSubmit = async () => {
 </script>
 
 <template>
-  <section class="my-4 w-full" @click="(e) => e.stopPropagation()">
+  <section class="my-4 w-full max-w-[500px] mx-auto" @click="(e) => e.stopPropagation()">
     <h2 v-if="!isEditSubscription" class="text-center my-4">Track new subscription</h2>
     <h2 v-else class="text-center my-4">Update subscription</h2>
 
     <form class="mx-5 space-y-3 w-[92%] mx-auto">
-      <TextinputField
-        :error="errors.provider"
-        name="subprovider"
-        :value="subprovider"
-        type="text"
-        size="medium"
-        label="Subscription Provider"
-        placeholder="Apple"
-        @type-event="
-          (value: string) => {
-            subprovider = value
-          }
-        "
-        :disabled="isSubmittingForm"
-      />
-      <TextinputField
-        :error="errors.type"
-        name="subtype"
-        :value="subtype"
-        type="text"
-        size="medium"
-        label="Subscription Type"
-        placeholder="Apple TV+"
-        @type-event="
-          (value: string) => {
-            subtype = value
-          }
-        "
-        :disabled="isSubmittingForm"
-      />
-
+      <div class="flex flex-col md:flex-row gap-3 items-stretch md:items-start">
+        <TextinputField
+          :error="errors.provider"
+          name="subprovider"
+          :value="subprovider"
+          type="text"
+          size="medium"
+          label="Subscription Provider"
+          placeholder="Apple"
+          @type-event="
+            (value: string) => {
+              subprovider = value
+            }
+          "
+          :disabled="isSubmittingForm"
+        />
+        <TextinputField
+          :error="errors.type"
+          name="subtype"
+          :value="subtype"
+          type="text"
+          size="medium"
+          label="Subscription Type"
+          placeholder="Apple TV+"
+          @type-event="
+            (value: string) => {
+              subtype = value
+            }
+          "
+          :disabled="isSubmittingForm"
+        />
+      </div>
       <TextinputField
         name="subdescription"
         :value="subdescription"
@@ -193,36 +192,40 @@ const handleSubmit = async () => {
         "
         :disabled="isSubmittingForm"
       />
-      <div class="w-1/2">
-        <TextinputField
-          type="date"
-          size="medium"
-          label="Subscription Start Date"
-          name="subdate"
-          :value="substartDate"
-          @type-event="
-            (value: string) => {
-              substartDate = value
+
+      <div class="flex flex-col md:flex-row gap-3 items-start">
+        <div class="w-[40%] md:w-2/6 shrink-0">
+          <TextinputField
+            type="date"
+            size="medium"
+            label="Subscription Start Date"
+            name="subdate"
+            :value="substartDate"
+            @type-event="
+              (value: string) => {
+                substartDate = value
+              }
+            "
+            :maxDate="getAccurateDateFormat(new Date())"
+            :disabled="isEditSubscription || isSubmittingForm"
+          />
+        </div>
+
+        <SelectField
+          :options="doesUserKnowsOptions"
+          :selected-option="selectUserKnowledge"
+          @select="
+            (option: SelectOption) => {
+              selectUserKnowledge = option
             }
           "
-          :maxDate="getAccurateDateFormat(new Date())"
-          :disabled="isEditSubscription || isSubmittingForm"
+          id="userKnowledge"
+          label="Choose an option you are sure of"
+          :disabled="isSubmittingForm"
+
+          className="w-1/2 grow-0"
         />
       </div>
-
-      <SelectField
-        :options="doesUserKnowsOptions"
-        :selected-option="selectUserKnowledge"
-        @select="
-          (option: SelectOption) => {
-            selectUserKnowledge = option
-          }
-        "
-        id="userKnowledge"
-        label="Kindly choose the option you feel most confident about"
-        :disabled="isSubmittingForm"
-      />
-
       <DurationorEnddate
         :datevalue="subendDate"
         :durationvalue="String(subduration)"
@@ -246,7 +249,6 @@ const handleSubmit = async () => {
         :minDate="getEndDate(substartDate, 3)"
         :disabled="isSubmittingForm"
         :error="errors.end_date"
-
       />
 
       <!-- <TextInputField /> -->
