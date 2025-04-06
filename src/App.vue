14d347df86a4a0d3ebd3e1651d956886a0cc5c1f
HayBeeCoder <haybeecodes@gmail.com>
2025-04-06 18:40:01 +0100

    [refactor] remove redundant code from App file

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
 <script setup lang="ts">
-import { RouterView, useRoute, type RouteRecordNameGeneric } from 'vue-router'
+import { RouterView } from 'vue-router'
 import TheHeader from './components/TheHeader.vue'
-import { onMounted } from 'vue'
-import { useThemeStore } from './stores/themesStore'
-import { watch } from 'vue'
-const { initializeTheme } = useThemeStore()
 
-const route = useRoute()
-onMounted(() => {
-  initializeTheme()
-  if (route.name == 'home') {
-    document.documentElement.classList.remove("dark")
-  }
-})
 
-watch(
-  () => route.name,
-  (newValue: RouteRecordNameGeneric) => {
-    if(newValue == "home"){
-
-      document.documentElement.classList.remove("dark")
-    }
-   
-  },
-)
 </script>
 
 <template>
